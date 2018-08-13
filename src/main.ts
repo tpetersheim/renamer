627b1e00841ef88d87b0c6eed04b18aa538d980a
Travis Petersheim <travispetersheim@gmail.com>
2018-08-12 22:17:29 -0500

    start of something

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,69 @@
-/**
- * Some predefined delays (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+import fs from 'fs';
+import yargs from 'yargs';
+
+// /**
+//  * Some predefined delays (in milliseconds).
+//  */
+// export enum Delays {
+//   Short = 500,
+//   Medium = 2000,
+//   Long = 5000,
+// }
+
+// /**
+//  * Returns a Promise<string> that resolves after given time.
+//  *
+//  * @param {string} name - A name.
+//  * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
+//  * @returns {Promise<string>}
+//  */
+// function delayedHello(name: string, delay: number = Delays.Medium): Promise<string> {
+//   return new Promise(
+//     (resolve: (value?: string) => void) => setTimeout(
+//       () => resolve(`Hello, ${name}`),
+//       delay,
+//     ),
+//   );
+// }
+
+// // Below are examples of using TSLint errors suppression
+// // Here it is suppressing missing type definitions for greeter function
+
+// export async function greeter(name) { // tslint:disable-line typedef
+//   // tslint:disable-next-line no-unsafe-any no-return-await
+//   return await delayedHello(name, Delays.Long);
+// }
+
+class Main {
+
+  // constructor()
+  // {
+
+  // }
+
+  public static Start() {
+    const args = yargs.argv;
+    const fileRenamesJsonArg = args.fileRenames;
+    const folderNameArg = args.folder;
+
+    const fileRenames = fs.readFileSync(fileRenamesJsonArg).toJSON();
+    Logger.Log(JSON.stringify(fileRenames));
+
+    const existingFileNames = fs.readdirSync(folderNameArg);
+    Logger.Log(JSON.stringify(existingFileNames));
+
+  }
 }
 
-/**
- * Returns a Promise<string> that resolves after given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(name: string, delay: number = Delays.Medium): Promise<string> {
-  return new Promise(
-    (resolve: (value?: string) => void) => setTimeout(
-      () => resolve(`Hello, ${name}`),
-      delay,
-    ),
-  );
-}
+Main.Start();
 
-// Below are examples of using TSLint errors suppression
-// Here it is suppressing missing type definitions for greeter function
+class Logger {
 
-export async function greeter(name) { // tslint:disable-line typedef
-  // tslint:disable-next-line no-unsafe-any no-return-await
-  return await delayedHello(name, Delays.Long);
+  public static Log(message: string) {
+    console.log(message);
+  }
+
+  public static Error(message: string) {
+    console.error(message);
+  }
 }
